88760e9a685f58665218c96add4fbfdeebc4c631
yurikarpenko <yurikarpenko@192.168.1.10>
2019-02-14 00:33:35 -0800

    initial check-in of the MothershipNGP code

---
+++ MothershipNGP/src/main/java/com/ngpstoreusa/mothership/MothershipNgpApplication.java
@@ -0,0 +1,14 @@
+package com.ngpstoreusa.mothership;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MothershipNgpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MothershipNgpApplication.class, args);
+	}
+
+}
+
---
+++ MothershipNGP/src/test/java/com/ngpstoreusa/mothership/MothershipNgpApplicationTests.java
@@ -0,0 +1,17 @@
+package com.ngpstoreusa.mothership;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MothershipNgpApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
